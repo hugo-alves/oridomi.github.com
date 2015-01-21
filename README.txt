2d06d26385376ad547dc10a67ebead43e21aa23d
hugo.alves <goninja@gmail.com>
2015-01-21 17:27:05 +0000

    com o oridomi

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/wBdQpy.
+
+ 
+
+Forked from [Kristen Overberg](http://codepen.io/kayeouh/)'s Pen [cFwxm](http://codepen.io/kayeouh/pen/cFwxm/).
---
+++ js/index.js
@@ -0,0 +1,17 @@
+var folded = new OriDomi('.paper', {
+  speed:           2000,  
+  ripple:          4,     // backwards ripple effect when animating
+  perspective:     4000,   // smaller values exaggerate 3D distortion
+  maxAngle:        180,    //fold range
+  vPanels:         12, 
+  hPanels:         1, 
+  shading:         'soft', 
+  touchEnabled:    true, 
+  touchSensitivity: .10
+  
+});
+
+//weird effects - comment/uncomment/switch orders/change values for MAXIMUM FUN
+
+folded.stairs(43);
+
