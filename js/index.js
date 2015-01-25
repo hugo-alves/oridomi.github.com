bb7f04342dd96b918903a4589c326b40ce867191
hugo.alves <goninja@gmail.com>
2015-01-25 14:55:33 +0000

    com a minha foto com a joao e com 10 fatias

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 var folded = new OriDomi('.paper', {
   speed:           3000,  
   ripple:          4,     // backwards ripple effect when animating
-  perspective:     4000,   // smaller values exaggerate 3D distortion
+  perspective:     6000,   // smaller values exaggerate 3D distortion
   maxAngle:        180,    //fold range
-  vPanels:         12, 
+  vPanels:         20, 
   hPanels:         1, 
   shading:         'soft', 
   touchEnabled:    true, 
@@ -13,5 +13,5 @@ var folded = new OriDomi('.paper', {
 
 //weird effects - comment/uncomment/switch orders/change values for MAXIMUM FUN
 
-folded.stairs(43).wait(2000).stairs(44, 'right');
+folded.stairs(43).wait(2000).stairs(44, 'right').accordion(45);
 
